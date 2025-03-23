663d5a41b818e2fa334e1f4e3ab0fbb9e792c519
Jimwelll Calma <jimwelllcalma@Jimwellls-MacBook-Air.local>
2025-03-24 00:03:03 +0800

    Fix: NAmes for models and routes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const eventsRoutes = require('./routes/events');
+const contactsRoutes = require('./routes/contacts');
+const adminRoutes = require('./routes/admins');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/events', eventsRoutes);
+app.use('/api/messages', contactsRoutes);
+app.use('/api/admin', adminRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB Atlas');
+  })
+  .catch(err => console.error('❌ MongoDB Connection Error:', err));
+
+
+module.exports = app; 
